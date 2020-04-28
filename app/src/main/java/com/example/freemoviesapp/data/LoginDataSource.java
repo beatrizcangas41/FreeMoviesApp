95d1d7f35d7005a08f5606bef42af08e0701d0ea
Beatriz Cangas Perez <38326583+beatrizcangas41@users.noreply.github.com>
2020-04-28 19:30:20 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/freemoviesapp/data/LoginDataSource.java
@@ -0,0 +1,29 @@
+package com.example.freemoviesapp.data;
+
+import com.example.freemoviesapp.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result.Error login(String username, String password) {
+
+        try {
+            // TODO: handle loggedInUser authentication
+            LoggedInUser fakeUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            "Jane Doe");
+            return new Result.Error(fakeUser);
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
