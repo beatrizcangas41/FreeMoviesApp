22982cf52492f1589e79b9c25ebe8bb76e40a98f
Beatriz Cangas Perez <38326583+beatrizcangas41@users.noreply.github.com>
2020-05-06 17:47:08 -0400

    Movies - Final

--- app/src/main/java/com/example/freemoviesapp/ui/WelcomePage.java
+++ app/src/main/java/com/example/freemoviesapp/ui/WelcomePage.java
@@ -3,9 +3,7 @@ package com.example.freemoviesapp.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.freemoviesapp.R;
 
 public class WelcomePage extends AppCompatActivity {
@@ -15,11 +13,13 @@ public class WelcomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_page);
 
-
-        new Handler().postDelayed(() -> {
-            final Intent mainIntent = new Intent(WelcomePage.this, MainActivity.class);
-            startActivity(mainIntent);
-            finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent mainIntent = new Intent(WelcomePage.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
         }, 3000);
     }
 }
